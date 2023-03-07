3717661ef3e769a9dfd854d0481cf2af7883e6f2
Kamilla <kaka.caliman@gmail.com>
2023-03-07 12:18:21 -0300

    feat: models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=127)
+    category = models.CharField(max_length=127)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.CharField(max_length=127, blank=True)
+
+    seller = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+
+    def __repr__(self) -> str:
+        return f"<Product ({self.id}) - {self.name}>"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    image = models.CharField(max_length=127, blank=True)
+    email = models.EmailField(
+        max_length=127,
+        unique=True,
+        error_messages={
+            "unique": "A user with that email already exists.",
+        }
+    )
+    is_seller = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"<User [{self.id}] - {self.first_name}>"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=127)
+    number = models.IntegerField()
+    city = models.CharField(max_length=127)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=8)
+    country = models.CharField(max_length=127)
+
+    user = models.OneToOneField(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='address'
+    )
+
+    def __repr__(self) -> str:
+        return f"<Address ({self.id}) - {self.street}>"
