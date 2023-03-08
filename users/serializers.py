1d64d24d05eec36a45c9f0cbb20f0b01dbaa06f0
Bryan Caua Custodio da Silva <103138568+Bryan-Caua@users.noreply.github.com>
2023-03-08 11:04:45 -0300

    feat/finalizando

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_superuser(**validated_data)
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.set_password(validated_data["password"])
+        instance.save()
+
+        return instance
+
+    class Meta:
+        model = User
+        fields = [
+                    "id",
+                    "first_name",
+                    "last_name", 
+                    "image",
+                    "email",                    
+                    "is_seller",
+        ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics
+from .serializers import UserSerializer
+from .permissions import IsAdminOrSellerOrReadOnly
 
-# Create your views here.
+class UserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminOrSellerOrReadOnly]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
