1b1cf8585c4b422097bdac0e85741263b8256b47
Bryan Caua Custodio da Silva <103138568+Bryan-Caua@users.noreply.github.com>
2023-03-10 11:13:09 -0300

    feat/atualizaçãoEndereço

--- users/views.py
+++ users/views.py
@@ -8,11 +8,6 @@ from .permissions import IsAdminOrSellerOrReadOnly
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # def create(self, request, *args, **kwargs):
-    #     adress_value = self.request.data.get("adress", False) 
-    #     if not adress_value:
-    #         return response({"msg": "adress is necessary"}, status.HTTP_400_BAD_REQUEST)     
-    #     return super().create(request, *args, **kwargs)
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
